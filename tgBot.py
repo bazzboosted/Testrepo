2e75dba6c7b958410c92a65bceb88a3c559f321f
rimoray <yarshagd@gmail.com>
2026-03-12 21:58:44 +0300

    работающая датабаза

--- tgBot.py
+++ tgBot.py
@@ -1,16 +1,28 @@
+
 import asyncio
 import json
 from aiogram import Bot, Dispatcher, F
 from aiogram.fsm.state import State, StatesGroup
 from aiogram.fsm.context import FSMContext
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
+import sqlite3
 import os
 
-script_dir = os.path.dirname(os.path.abspath(__file__))  # Папка с tgBot.py
+
+script_dir    = os.path.dirname(os.path.abspath(__file__))
 receipts_path = os.path.join(script_dir, "receipts.json")
 
 with open(receipts_path, "r", encoding="utf-8") as f:
     recipes = json.load(f)
+    
+path = r"C:\Git\Project\botDatabase"
+conn = sqlite3.connect(path)
+cursor = conn.cursor()
+
+cursor.execute("select * from dishes")
+rows = cursor.fetchall()
+for row in rows:
+    print(row)
 
 bot = Bot("8261198757:AAH6zT0tMVsfrud1Wq09sZ2x851jBt7tnqQ")
 dp = Dispatcher()
@@ -21,7 +33,8 @@ def GetKeyboard():
         [
             [KeyboardButton(text = "чай"), KeyboardButton(text = "суп"),
              KeyboardButton(text = "тушенка совок"), KeyboardButton(text = "лазанья"),
-             KeyboardButton(text = "компот")
+             KeyboardButton(text = "компот"),
+             KeyboardButton(text = "Избранное")
              ]
             ])
     resize_keyboard = True
@@ -31,6 +44,7 @@ def GetKeyboard():
 class States(StatesGroup):
     nameState = State()
     choosingDishState = State()
+    afterChoosingState = State()
   
 @dp.message(F.text == "/start")
 async def FirstAnswer(msg, state: FSMContext):
@@ -49,10 +63,14 @@ async def Receipts(msg, state: FSMContext):
     if (dish in recipes):
         await msg.answer(recipes[dish])
         await msg.answer("вам понравилось?" + name)
+        await state.set_state(States.afterChoosingState)
     else:
         await msg.answer("выберите то, что на кнопках")
     
+    
+    
 
+    
 
 
 asyncio.run(dp.start_polling(bot))
